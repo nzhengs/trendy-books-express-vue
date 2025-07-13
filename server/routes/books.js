0638e885c5662af4bd5991461e29c5958bf0928e
Anjana Hengaju <45703218+nzhengs@users.noreply.github.com>
2025-07-13 17:09:05 -0400

    Extract common api handler

--- server/routes/books.js
+++ server/routes/books.js
@@ -3,52 +3,42 @@ require("dotenv").config();
 var router = express.Router();
 
 const OVERVIEW_URL = "https://api.nytimes.com/svc/books/v3/lists/overview.json";
-const LIST_URL = "https://api.nytimes.com/svc/books/v3/lists/current/{listName}.json";
+const LIST_URL =
+  "https://api.nytimes.com/svc/books/v3/lists/current/{listName}.json";
 
-
-router.get("/lists", function (req, res, next) {
-  const url = OVERVIEW_URL + "?api-key=" + process.env.NYT_API_KEY;
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((response) => {
-      if (response.status === "OK") {
-        res.json(response.results.lists);
-      } else {
-        res.status(500).json({ error: "Failed to fetch best sellers lists." });
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching best sellers:", error);
-      res.status(500).json({ error: "Internal server error." });
-    });
+router.get("/lists", function (req, res) {
+  fetchBooksApi(OVERVIEW_URL, res, (response) => response.results.lists);
 });
 
-router.get("/lists/:listName", function (req, res, next) {
-  const url = LIST_URL.replace("{listName}", req.params.listName) + "?api-key=" + process.env.NYT_API_KEY;
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        console.log(response)
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((response) => {
-      if (response.status === "OK") {
-        res.json(response.results);
-      } else {
-        res.status(500).json({ error: "Failed to fetch best sellers lists." });
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching best sellers:", error);
-      res.status(500).json({ error: "Internal server error." });
-    });
+router.get("/lists/:listName", function (req, res) {
+  const url = LIST_URL.replace("{listName}", req.params.listName);
+  fetchBooksApi(url, res, (response) => response.results);
 });
 
 module.exports = router;
+
+function fetchBooksApi(url, res, responseMapper) {
+  fetch(withApiKey(url))
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          "Unable to fetch data from API: " + response.statusText
+        );
+      }
+      return response.json();
+    })
+    .then((response) => {
+      if (response.status === "OK") {
+        res.json(responseMapper(response));
+      } else {
+        res.status(500).json({ error: "API response was not ok" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Internal server error." });
+    });
+}
+
+function withApiKey(url) {
+  return url + "?api-key=" + process.env.NYT_API_KEY;
+}
