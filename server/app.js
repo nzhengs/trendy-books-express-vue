c26e0ec0834d99bbda407a6d6285bcbe907cb8a3
Anjana Hengaju <45703218+nzhengs@users.noreply.github.com>
2025-07-12 23:17:56 -0400

    Create /api/books with sample nyt response

--- server/app.js
+++ server/app.js
@@ -3,8 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -14,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', require('./routes/index'));
+app.use('/api/books', require('./routes/books'));
 
 module.exports = app;
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const response =  require('./nyt-response.json');
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(response);
 });
 
 module.exports = router;
